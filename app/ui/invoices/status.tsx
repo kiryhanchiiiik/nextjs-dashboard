601b05109dbdc73b72a440b296acf062f8cb2acf
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-11-02 13:22:35 +0200

    Improving Accessibility 2

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,24 +1,29 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+"use client";
+import { createInvoice, State } from "@/app/lib/actions";
+import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import { useActionState } from "react";
 
 export default function InvoiceStatus({ status }: { status: string }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
+          "bg-gray-100 text-gray-500": status === "pending",
+          "bg-green-500 text-white": status === "paid",
+        }
       )}
     >
-      {status === 'pending' ? (
+      {status === "pending" ? (
         <>
           Pending
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === "paid" ? (
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
